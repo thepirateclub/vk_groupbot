528349fe19df2b39f660c2f92ad702ac9655c9e0
The Pirate <github@the-pirate.ru>
2020-08-25 14:36:54 +0400

    Обновление testController.js
    Удалено подключение класса Query

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,6 +1,5 @@
 const EventController = require("./eventController");
 const Log = require('../core/log');
-const Query = require('../core/query');
 
 class TestController extends EventController {
 
